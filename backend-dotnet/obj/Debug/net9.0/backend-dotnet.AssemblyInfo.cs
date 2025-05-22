b370d0d903683f34666ec3dfaefa22b0197dd8d7
paulb <paulberca@gmail.com>
2025-05-22 15:18:24 +0300

    dotnet backend unstaged files

--- backend-dotnet/obj/Debug/net9.0/backend-dotnet.AssemblyInfo.cs
+++ backend-dotnet/obj/Debug/net9.0/backend-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59791e5ce750b54b287a7a69e0d602dbb97ffca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bc919b5b1c7db730a15939c81789e9bc5ada9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
